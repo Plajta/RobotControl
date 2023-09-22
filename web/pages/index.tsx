cf2d8db1c12911c4f8b3bb1b75425e5b47919510
JsSusenka <susenky.opavia@gmail.com>
2023-09-23 00:30:49 +0200

    socketio???

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,13 +1,41 @@
+import { useState, useEffect } from "react";
 import { Grid, Text } from "@mantine/core";
+import { socket } from "~/modules/socket";
 import { Card } from "~/components/Card";
 import { Layout } from "~/components/Layout";
 
 export default function Home() {
+	const [isConnected, setIsConnected] = useState(socket.connected);
+
 	function getCameraFeed() {
 		return process.env.NODE_ENV === "development"
 			? "https://www.chmi.cz/files/portal/docs/meteo/kam/plzen.jpg"
 			: "http://localhost:6969/video";
 	}
+
+	useEffect(() => {
+		function onConnect() {
+			setIsConnected(true);
+		}
+
+		function onDisconnect() {
+			setIsConnected(false);
+		}
+
+		function onFooEvent(value: any) {
+			console.log(value);
+		}
+
+		socket.on("connect", onConnect);
+		socket.on("disconnect", onDisconnect);
+		socket.on("foo", onFooEvent);
+
+		return () => {
+			socket.off("connect", onConnect);
+			socket.off("disconnect", onDisconnect);
+			socket.off("foo", onFooEvent);
+		};
+	}, []);
 
 	return (
 		<Layout>
--- web/webBuild/_next/static/JqgszIBcTvdJMB6Xw-9LX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/443-2dc5e587c6a87ca3.js","static/chunks/pages/index-d74d32c71daaf8e2.js"],"/_error":["static/chunks/pages/_error-1204c3bd00f1a964.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
