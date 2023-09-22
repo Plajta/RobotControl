f8d1d5869bd32a06a5725384d6f828aa041a1976
JsSusenka <susenky.opavia@gmail.com>
2023-09-22 18:07:07 +0200

    renderovani webu pres flask

--- src/render.py
+++ src/render.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Initializing flask app
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../web/out/_next", template_folder="../web/out")
 
 # Route for seeing a data
 @app.route('/')
 def hello_world():
-   return "Hello World"
+    return render_template("index.html")
+
+app.debug=True
+app.run(host='0.0.0.0', port=6969)
