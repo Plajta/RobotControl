ac2ccd789565abfce3f68fdca8cebeb56c96723c
HelloWorld7894 <danpojhan@protonmail.com>
2023-09-22 19:30:34 +0200

    added OOP

--- src/program.py
+++ src/program.py
@@ -1,5 +1,18 @@
 from render import app
-import robot_control
+from robot_control import Robot
+
+import cv2
 
 def main():
-    pass
+    robot = Robot()
+    robot.camera_init()
+    while True:
+        img = robot.get_frame()
+        cv2.imshow("frame", img)
+        if ord("q") == cv2.waitKey(1):
+            robot.camera_stop()
+            robot.disconnect_robot()
+
+
+if __name__ == "__main__":
+    main()
--- src/robot_control.py
+++ src/robot_control.py
@@ -9,105 +9,43 @@ import time
 host = "192.168.2.1"
 port = 40923
 
-#robot object
-ep_robot = None
-ep_camera = None
+class Robot:
+    def __init__(self):
+        ep_robot = robot.Robot()
+        ep_robot.initialize(conn_type="ap")
 
-def robot_init():
-    ep_robot = robot.Robot()
-    ep_robot.initialize(conn_type="ap")
+        ep_version = ep_robot.get_version()
+        print("Robot Version: {0}".format(ep_version))
 
-    ep_version = ep_robot.get_version()
-    print("Robot Version: {0}".format(ep_version))
+        self.ep_robot = ep_robot
+        self.ep_camera = None
 
-    #time.sleep(5)
-    #ep_robot.close()
-    #exit(0)
-    return ep_robot
+    #robot methods
+    def disconnect_robot(self):
+        self.ep_robot.close()
+        exit(0)
 
-def disconnect_robot():
-    ep_robot.close()
-    exit(0)
+    #camera methods
+    def camera_init(self):
+        self.ep_camera = self.ep_robot.camera
+        self.ep_camera.start_video_stream(display=False)
 
-def connect():
-    address = (host, int(port))
+    def camera_stop(self):
+        cv2.destroyAllWindows()
+        self.ep_camera.stop_video_stream()
 
-    # Establish a TCP connection with the control command port of the robot.
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    print("Connecting...")
-
-    s.connect(address)
-
-    print("Connected!")
-
-    return s
-
-def send_cmd(s, cmd):
-
-    # When the user enters Q or q, exit the current program.
-    if msg.upper() == 'Q':
-            disconnect_socket(s)
-
-    # Add the ending character.
-    msg += ';'
-    
-    # Send control commands to the robot.
-    s.send(msg.encode('utf-8'))
-
-    try:
-            # Wait for the robot to return the execution result.
-            buf = s.recv(1024)
-            print(buf.decode('utf-8'))
-    except socket.error as e:
-            print("Error receiving :", e)
-            sys.exit(1)
-    if not len(buf):
-            disconnect_socket(s)
-
-def disconnect_socket(s):
-    # Disconnect the port connection.
-    s.shutdown(socket.SHUT_WR)
-    s.close()
-
-#camera settings
-def camera_init():
-    ep_camera = ep_robot.camera
-    ep_camera.start_video_stream(display=False)
-    return ep_camera
-
-def camera_stop():
-    cv2.destroyAllWindows()
-    ep_camera.stop_video_stream()
-
-def get_frame():
-    frame = ep_camera.read_cv2_image()
-    return frame
+    def get_frame(self):
+        frame = self.ep_camera.read_cv2_image()
+        return frame
 
 if __name__ == "__main__":
     
     print("testing")
-    ep_robot = robot_init()
-    ep_camera = camera_init()
+    robot = Robot()
+    robot.camera_init()
     while True:
-        img = get_frame()
+        img = robot.get_frame()
         cv2.imshow("frame", img)
         if ord("q") == cv2.waitKey(1):
-            camera_stop()
-            disconnect_robot()
-
-    """
-    ep_robot = robot.Robot()
-    ep_robot.initialize(conn_type="ap")
-
-    ep_camera = ep_robot.camera
-
-    # 显示200帧图传
-    ep_camera.start_video_stream(display=False)
-    for i in range(0, 200):
-        img = ep_camera.read_cv2_image()
-        cv2.imshow("Robot", img)
-        cv2.waitKey(1)
-    cv2.destroyAllWindows()
-    ep_camera.stop_video_stream()
-    """
+            robot.camera_stop()
+            robot.disconnect_robot()
