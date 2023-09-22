2a5ae36ca38c7942d509cf7c6e5ee6d7bae15dec
JsSusenka <susenky.opavia@gmail.com>
2023-09-22 17:52:07 +0200

    prasarna ale whathever

---
+++ web/out/_next/static/-PTEVN9zLMXe0ybO6W4CF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-b04ade9af88b5c81.js"],"/_error":["static/chunks/pages/_error-1204c3bd00f1a964.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
