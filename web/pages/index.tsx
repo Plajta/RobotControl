89ea0a29020e173c97b353d0a02e036954bec82d
JsSusenka <susenky.opavia@gmail.com>
2023-09-22 21:50:19 +0200

    kamera feed s placeholderem

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,8 +1,14 @@
-import { Grid } from "@mantine/core";
+import { Grid, Text } from "@mantine/core";
 import { Card } from "~/components/Card";
 import { Layout } from "~/components/Layout";
 
 export default function Home() {
+	function getCameraFeed() {
+		return process.env.NODE_ENV === "development"
+			? "https://www.chmi.cz/files/portal/docs/meteo/kam/plzen.jpg"
+			: "http://localhost:6969/video";
+	}
+
 	return (
 		<Layout>
 			<Grid h="100%">
@@ -11,7 +17,19 @@ export default function Home() {
 				</Grid.Col>
 
 				<Grid.Col span={8}>
-					<Card height="88vh">2</Card>
+					<Card
+						height="88vh"
+						divider
+						header={{
+							leftSection: <Text fw="bold">Red Bull kamera</Text>,
+						}}
+					>
+						<img
+							src={getCameraFeed()}
+							alt="camera feed"
+							width="100%"
+						/>
+					</Card>
 				</Grid.Col>
 
 				<Grid.Col span={2}>
