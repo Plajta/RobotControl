eef398d8b871bd51829d5ea2563166a1fd5ef3d8
HelloWorld7894 <danpojhan@protonmail.com>
2023-09-22 23:57:20 +0200

    added dumb socketio

--- src/render.py
+++ src/render.py
@@ -1,10 +1,11 @@
 import cv2
 from flask import Flask, Response, render_template
+from flask_socketio import SocketIO, emit
 
 from program import main
 
 app = Flask(__name__, static_folder="../web/webBuild/_next", template_folder="../webBuild/out")
-
+socketio = SocketIO(app)
 
 @app.route("/")
 def hello_world():
@@ -15,6 +16,9 @@ def hello_world():
 def video():
     return Response(main(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
 app.debug = True
-app.run(host="0.0.0.0", port=6969)
+#app.run(host="0.0.0.0", port=6969)
+socketio.run(app, host='0.0.0.0', port=6969)
+
+while True:
+    emit('data-emit', {'data': 'Connected'})
