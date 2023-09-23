c9bcfa397c685559e842b681b0bbeef766aefeab
HelloWorld7894 <danpojhan@protonmail.com>
2023-09-23 05:17:53 +0200

    backend fix

--- src/comm.py
+++ src/comm.py
@@ -11,6 +11,8 @@ class DataComm:
         data_dict["start_objects"] = start_objects
         data_dict["battery"] = battery
 
+        print(data_dict)
+
         self.data_queue.put(data_dict)
 
     def get_data(self):
