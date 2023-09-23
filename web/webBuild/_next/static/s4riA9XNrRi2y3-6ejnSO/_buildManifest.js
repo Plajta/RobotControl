a103df2c73348ba08928aee29ed954b95fc1beb6
JsSusenka <susenky.opavia@gmail.com>
2023-09-23 03:42:17 +0200

    tab styly

---
+++ web/webBuild/_next/static/s4riA9XNrRi2y3-6ejnSO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/691-1c9af7a1f0de94cd.js","static/css/69d9b6b66b8f2851.css","static/chunks/pages/index-7d8636fa4f0c9aea.js"],"/_error":["static/chunks/pages/_error-1204c3bd00f1a964.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
